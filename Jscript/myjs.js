dbdf6fa1cd1b0f542d905e8f5cc9c6d4a112e0e4
Umberto <umbertopellegrino96@gmail.com>
2020-06-15 15:33:47 +0200

    if con isNaN

---
+++ Jscript/myjs.js
@@ -0,0 +1,30 @@
+var bigliettoScontato;
+
+//chilometri che vuole percorrere
+var kmDaPercorrere = parseInt(prompt ("Per iniziare il viaggio dovrai dirmi quanti km dobbiamo percorrere:"), 10);
+console.log(kmDaPercorrere);
+if (isNaN(kmDaPercorrere)) {
+ kmDaPercorrere = parseInt(prompt ("Il dato inserito non è valido, riprovare:"), 10);
+}
+
+// e l’età del passeggero
+var etaCliente = parseInt(prompt ("Inseriamo la tua età:" ), 10);
+if (isNaN(kmDaPercorrere)) {
+ etaCliente = parseInt(prompt ("Il dato inserito non è valido, riprovare:"), 10);
+}
+
+//il prezzo del biglietto è definito in base ai km (0.21 € al km)
+var costoBiglietto = kmDaPercorrere * 0.21;
+
+
+if (etaCliente < 18) {
+  // a applicato uno sconto del 20% per i minorenni
+  bigliettoScontato = ((costoBiglietto * 80)/ 100);
+  document.getElementById("scontato").innerHTML=("Ma vista la tua età il prezzo in realtà è: " + bigliettoScontato +"€")
+}else if (etaCliente > 65) {
+  //va applicato uno sconto del 40% per gli over 65
+  bigliettoScontato = ((costoBiglietto * 60)/ 100);
+  document.getElementById('scontato').innerHTML=("Ma vista la tua età il prezzo in realtà è: " + bigliettoScontato +"€")
+}
+
+document.getElementById('costo-biglietto').innerHTML=("Il costo del tuo biglietto è: " + costoBiglietto +"€")
